1622ca8d09b5f1feefb26b96a7b0e23f7a62a337
Sengeki1 <gamersengeki@gmail.com>
2025-10-07 11:32:32 -0100

    echalon form

--- 01/guassmethod.py
+++ 01/guassmethod.py
@@ -1,13 +1,5 @@
 from ..utils.main import Matrix3, Vector3     
 
-matrix = Matrix3(
-    Vector3(0,  1,  3), 
-    Vector3(0,  0,   2),
-    Vector3(3,  1,   2),
-)
-const = Vector3(1, 10, 6)
-matrix.add_const(const)
-
 class GausMethod:
     def __init__(self, matrix: Matrix3):
         m_length = len(matrix.m)
@@ -15,18 +7,56 @@ class GausMethod:
         self.c = matrix.c
 
         # swap if zeros found given the size of the matrix
-        if m_length < 3:
-            if self.m[0][0] < 1:
-                self.swap(0, self.m[0][0])
-            elif self.m[0][1] < 1:
-                self.swap(1, self.m[0][1])
-        else:
-            for _ in range(m_length):
-                for i, expr in enumerate(self.m):
-                    for j, _ in enumerate(expr):
-                        if self.m[i][j] == 0:
-                            self.swap(i)
+        for _ in range(m_length):
+            for i, row in enumerate(self.m):
+                for j, _ in enumerate(row):
+                    if self.m[i][j] == 0:
+                        self.swap(i)
+
+        # transform to echalon form
+        col_index = 0
+        for _ in range(m_length):
+            if m_length < 3:
+                r_c = [
+                        self.m[0][col_index], 
+                        self.m[1][col_index]
+                      ]
+            else:
+                r_c = [
+                        self.m[0][col_index], 
+                        self.m[1][col_index], 
+                        self.m[2][col_index]
+                      ]
             
+            for i, c in enumerate(r_c):
+                if col_index == 0:
+                
+                    if c != 0 and (i == 1 or i == 2):
+                        k = c / self.m[0][0]
+                        f_m = [float(i) * k for i in self.m[0]]
+                        self.m[i] = [x - y for x, y in zip(self.m[i], f_m)]
+                        self.c[0][i] -= k * self.c[0][0]
+                                            
+                elif col_index == 1:
+                    if c != 0 and (i == 2):
+                        k = c / self.m[1][1]
+                        f_m = [float(i) * k for i in self.m[1]]
+                        self.m[i] = [x - y for x, y in zip(self.m[i], f_m)]
+                        self.c[0][i] -= k * self.c[0][1]
+
+            col_index += 1
+
+        if self.m[1][1] != 1.0:
+            k_ = (1.0 / self.m[1][1])
+            self.m[1] = [k_ * i for i in self.m[1]]
+            self.c[0][1] *= k_
+        
+        if m_length == 3:
+            if self.m[2][2] != 1.0:
+                k_ = (1.0 / self.m[2][2])
+                self.m[2] = [k_ * i for i in self.m[2]]
+                self.c[0][2] *= k_
+
         print(f"[Matrix] - {self.m}")
         print(f"[Constants] - {self.c}")
 
@@ -37,12 +67,18 @@ class GausMethod:
         if (self.m[pos] != self.m[-1]):
             self.m[(pos + 1) % len(self.m)] = self.m[pos]
             self.m[pos] = prev_v
-        print(f"[{pos}] - {self.m}")
-        print(f"[{pos}] - {self.c}")
 
         self.c[0][(pos + 1) % len(self.m)] = self.c[0][pos]
         self.c[0][pos] = prev_c
 
+matrix = Matrix3(
+    Vector3(1,  1,  -1), 
+    Vector3(2,  -1,   1)
+)
+const = Vector3(-2, 5, 0)
+matrix.add_const(const)
+
 GausMethod(matrix)
+
 
 
